d57ea27009b859b4e7d75db8fcf8c35917fffa8d
Evan Saju Mathew <evanptc@gmail.com>
2024-11-24 21:16:26 +0530

    Update solution_for_netflix_sql_questions.sql

--- solution_for_netflix_sql_questions.sql
+++ solution_for_netflix_sql_questions.sql
@@ -20,14 +20,34 @@
     ORDER BY year_added ASC
 
 -- Q)4 Top Countries by Content
-    
+    SELECT 
+        TRIM(country_name) AS country, 
+        COUNT(*) AS content_count
+    FROM (
+        SELECT 
+            UNNEST(string_to_array(country, ',')) AS country_name
+        FROM 
+            netflix
+    ) AS countries
+    GROUP BY 
+        TRIM(country_name)
+    ORDER BY 
+        content_count DESC;
+   
+-- Q)5 Recent Additions
+    SELECT title ,date_added FROM netflix
+    WHERE date_added IS NOT NULL
+    ORDER BY date_added DESC
+      
+-- Q)6 Longest Movie or TV Show
+    SELECT title ,duration, type FROM netflix
+    WHERE duration IS NOT NULL
+    ORDER BY duration DESC, title ASC
 
+-- Q)7 Specific Genre Analysis
     
-Recent Additions
-Longest Movie or TV Show
-Specific Genre Analysis
-Count the number of Movies vs TV Shows
-Find the most common rating for movies and TV shows
-List all movies released in a specific year (e.g., 2020)
-List all movies that are documentaries
-Find content added in the last 5 years
+-- Q)8 Count the number of Movies vs TV Shows
+-- Q)9 Find the most common rating for movies and TV shows
+-- Q)10 List all movies released in a specific year (e.g., 2020)
+-- Q)5List all movies that are documentaries
+-- Q)5Find content added in the last 5 years
